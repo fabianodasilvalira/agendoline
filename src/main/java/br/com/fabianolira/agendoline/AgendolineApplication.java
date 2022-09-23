cdf11e793fdee85b010d7b13458f91966d8cda53
fabianoLira <fabianolira@cge.pi.gov.br>
2022-09-23 08:44:57 -0300

    first commit

---
+++ src/main/java/br/com/fabianolira/agendoline/AgendolineApplication.java
@@ -0,0 +1,13 @@
+package br.com.fabianolira.agendoline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendolineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendolineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fabianolira/agendoline/AgendolineApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fabianolira.agendoline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendolineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
