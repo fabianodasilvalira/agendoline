0237ccd613978c63182ef4994384c9301028e95e
fabianodasilvalira <fabiano.lira@objetivopi.com.br>
2022-10-03 21:47:29 -0300

    implantando swagger2

--- src/main/java/br/com/fabianolira/agendoline/config/swagger/SwaggerConfigurations.java
+++ src/main/java/br/com/fabianolira/agendoline/config/swagger/SwaggerConfigurations.java
@@ -21,7 +21,7 @@ public class SwaggerConfigurations {
 	public Docket forumApi() {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
-				.apis(RequestHandlerSelectors.basePackage("br.com.alura.forum"))
+				.apis(RequestHandlerSelectors.basePackage("br.com.fabianolira.agendoline"))
 				.paths(PathSelectors.ant("/**"))
 				.build()
 				.ignoredParameterTypes(Usuario.class)
